18d83014d534d40de027ff33ac11544e61e6cae9
kazki1981 <kazki1981@gmail.com>
2026-08-10 10:53:43 +0900

    Fix mobile point registration overflow

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "grid-atlas-static-v232";
+const CACHE_NAME = "grid-atlas-static-v233";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
   "./src/styles.css?v=64",
-  "./src/main.js?v=224",
+  "./src/main.js?v=225",
   "./src/cloud-client.js?v=2",
   "./src/gridatlas-import.js?v=2",
   "./src/gridatlas-assets.js?v=1",
