daaa3477860317c8ba46932ef0d409b495655f7d
kazki1981 <kazki1981@gmail.com>
2026-08-11 23:47:09 +0900

    Fit mobile overview into two rows

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "grid-atlas-static-v270";
+const CACHE_NAME = "grid-atlas-static-v271";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
   "./src/styles.css?v=64",
-  "./src/main.js?v=262",
+  "./src/main.js?v=263",
   "./src/cloud-client.js?v=2",
   "./src/gridatlas-import.js?v=2",
   "./src/gridatlas-assets.js?v=1",
